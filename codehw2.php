57ef0cb1812447abebbdcdc243d03e53a34d4ebe
JoshuaDull <jdull@pratt.edu>
2015-09-22 01:08:54 -0400

    Made head and tails of it

--- codehw2.php
+++ codehw2.php
@@ -8,18 +8,29 @@
   <body>
 <?php
 
-$isbn = "123456789H";
+$isbn = "123456789X";
 $results = isbn_tester($isbn);
 if ($results == true)
   {
-    echo "<br> <p>$isbn</p>" . "<br>NOW THAT'S WHAT I CALL A GINUINE ISBN #<br><br>| m  |<br>|  o |<br>| r  |<br>|  e |<br>___info___<br>\|||||||/<br>\|||||/<br>\|||/<br>\|/<br>V<br>";
-    echo "<a href=http://www.isbnsearch.org/isbn/$isbn>L I N K</a>";
-  } else {
+    echo "<br> <p>$isbn</p>" . "<br>NOW THAT'S WHAT I CALL A GINUINE ISBN #<br><br><div>| m  |<br>|  o |<br>| r  |<br>|  e |<br>___info___<br>\|||||||/<br>\|||||/<br>\|||/<br>\|/<br>V<br></div>";
+    echo "<a href=http://www.isbnsearch.org/isbn/$isbn>L I N K</a><br><br><br>";
+  } elseif ($results == false)
+  {
     echo "NOPE";
   }
 
+echo "<div class=sonic> SONIC</div> or <div class=tails>TAILS</div><br>";
+echo sonic_or_tails(1) . "<br>";
+echo sonic_or_tails(3) . "<br>";
+echo sonic_or_tails(5) . "<br>";
+echo sonic_or_tails(7) . "<br>";
+echo sonic_or_tails(9) . "<br><br><br><br><br>";
+
+
+echo double_trouble();
+
 #--------FUNCTIONS----------
-#
+#isbn tester
 function isbn_tester($isbn)
 {
   if (preg_match("/^[0-9X]{10}$/", $isbn))
@@ -53,6 +64,49 @@ function isbn_tester($isbn)
 
 }}
 
+#sonic or tails generator
+
+function sonic_or_tails($input)
+{
+  for ($count = $input; $count >= 1; --$count)
+  {
+    if (mt_rand(1,2) == 1)
+    {
+      echo "<img class=result src=http://kandipatterns.com/images/patterns/characters/12081_Classic_Sonic_.png>";
+    } else {
+      echo "<img class=result src=http://kandipatterns.com/images/patterns/characters/12076_Classic_Tails.png>";
+    }
+  }
+}
+
+#double tails
+
+function double_trouble()
+{
+  $count = 0;
+  $x = 0;
+  $y = 1;
+  do {
+    ++$count;
+    $x = mt_rand(1,2);
+
+    if ($x == 1)
+    {
+      echo "<img class=result src=http://kandipatterns.com/images/patterns/characters/12081_Classic_Sonic_.png>";
+      $y = 1;
+    } else {
+      echo "<img class=result src=http://kandipatterns.com/images/patterns/characters/12076_Classic_Tails.png>";
+      if ($y == 1)
+      {
+        $y = 2;
+      } else {
+        $y = 3;
+      }
+
+    }
+  } while ($y != 3);
+  return "<h3 class=try> IT TOOK " . $count . " TRIES TO GET DOUBLE TAILS!</p>";
+}
  ?>
   </body>
   </html>
