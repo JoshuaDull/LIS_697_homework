11064a1c05438df914dea079ea688d172328f1f0
JoshuaDull <jdull@pratt.edu>
2015-09-22 21:13:16 -0400

    Week 5 classwork

---
+++ 9_22_2015.php
@@ -0,0 +1,33 @@
+<?php
+
+// $name = array("Joshua", "Lee", "Dull");
+
+// echo $name[1] . "<br>";
+// unset($name[1]);
+// echo "<pre>";
+// print_r($name);
+// echo "</pre><pre>";
+// $name[1] = "Lee";
+// print_r($name);
+// echo "</pre>";
+
+// $nom = array ("First"=>"Joshua", "Middle"=>"Lee", "Last"=>"Dull");
+// print_r($nom);
+// echo "<br>" . $nom['First'] . "<pre>";
+// unset($nom['Last']);
+// print_r($nom);
+// echo "</pre><pre>";
+// $nom['Middle'] = "Lee";
+// print_r($nom);
+// echo "</pre>";
+
+// foreach ($nom as $k => $v) {
+	// echo "<br>The value at $k is $v <br>";
+// }
+
+// foreach ($name as $k => $v) {
+	// echo "<br>The value at $k is $v <br>";
+	
+	
+}
+?>
