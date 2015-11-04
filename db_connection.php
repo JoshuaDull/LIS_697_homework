603c2c956384a681cde87d4d32890109a626c052
JoshuaDull <jdull@pratt.edu>
2015-11-03 21:08:28 -0500

    slight changes

--- db_connection.php
+++ db_connection.php
@@ -4,7 +4,7 @@ require_once 'login.php';
 $conn = new mysqli($hn, $un, $pw, $db);
 if ($conn->connect_error) die($conn->connect_error);
 
-$country = "'France'";
+/* $country = "'France'";
 $query = "SELECT * FROM `customers` WHERE country=$country";
 $result = $conn->query($query);
 if (!$result) die ($conn->error);
@@ -14,7 +14,7 @@ echo "<strong>Clients francais:</strong><br><br>";
 for ($j = 0 ; $j < $rows ; ++$j) {
 	$result->data_seek($j);
 	$row = $result->fetch_row();
-	echo $row[1]."<br>".$row[3]." ".$row[2]."<br>".$row[4]."<br>".$row[5]."<br>".$row[7].", ".$row[10]." ".$row[9]."<br><br>";
+	echo $row[1]."<br>".$row[3]." ".$row[2]."<br>".$row[4]."<br>".$row[5]."<br>" .$row[7].", ".$row[10]." ".$row[9]."<br><br>"; */
 }
 $result->close();
 $conn->close();
